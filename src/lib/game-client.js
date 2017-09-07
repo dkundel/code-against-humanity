2cd9939440559f97de1648ca09dc79d2dbec0464
Dominik Kundel <dominik.kundel@gmail.com>
2017-09-07 23:09:17 +0200

    :sparkles: Add sync game client

---
+++ src/lib/game-client.js
@@ -0,0 +1,114 @@
+import * as EventEmitter from 'event-emitter';
+import { stringify } from 'query-string';
+import { AccessManager } from 'twilio-common';
+import { SyncClient } from 'twilio-sync';
+
+let instance;
+class GameClient {
+  static shared() {
+    instance = instance || new GameClient();
+    return instance;
+  }
+
+  constructor() {
+    this.client = undefined;
+    this.username = undefined;
+    this.roomId = undefined;
+    this.identity = undefined;
+    this.game = undefined;
+    this.gameDoc = undefined;
+  }
+
+  async init(username, roomId) {
+    if (this.client) {
+      return this.client;
+    }
+
+    this.username = username;
+    this.roomId = roomId;
+
+    const { token, identity } = await this.fetchToken();
+    this.identity = identity;
+    this.accessManager = this.createAccessManager(token);
+    this.client = this.createClient(token);
+    this.gameDoc = this.client.document(roomId);
+    this.game = this.gameDoc.value;
+    this.addEventListeners();
+  }
+
+  getGame() {
+    return this.game;
+  }
+
+  get(key) {
+    return this.game[key];
+  }
+
+  set(key, value) {
+    if (!key) {
+      return;
+    }
+
+    this.gameDoc.update({
+      [key]: value
+    });
+  }
+
+  addEventListeners() {
+    this.gameDoc.on('updated', data => {
+      this.game = data;
+      this.emit('updated', { game: data });
+    });
+
+    this.gameDoc.on('updated', data => {
+      this.game = data;
+      this.emit('updated', { game: data });
+    });
+  }
+
+  createAccessManager(token) {
+    const accessManager = new AccessManager(token);
+    accessManager.on('tokenExpired', async () => {
+      const { token } = await this.fetchToken();
+      accessManager.updateToken(token);
+    });
+    accessManager.on('tokenUpdated', () => {
+      this.client.updateToken(this.accessManager.token);
+    });
+    return accessManager;
+  }
+
+  createClient(token) {
+    const client = new GameClient(token);
+    client.on('connectionStateChanged', ({ connectionState }) => {
+      if (
+        connectionState === 'disconnected' ||
+        connectionState === 'error' ||
+        connectionState === 'denied'
+      ) {
+        this.emit('disconnected');
+        this.client = undefined;
+      }
+    });
+    return client;
+  }
+
+  async fetchToken() {
+    const query = stringify({
+      username: this.username,
+      roomId: this.roomId
+    });
+    const url = `/api/token?${query}`;
+    const resp = await fetch(url);
+
+    if (!resp.ok) {
+      throw new Error('Failed to request token');
+    }
+
+    return resp.json();
+  }
+}
+
+EventEmitter(GameClient.prototype);
+
+export default GameClient;
