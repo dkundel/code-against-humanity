bf6404118601252c768b61c210cf4e5a885f3906
Dominik Kundel <dominik.kundel@gmail.com>
2017-09-08 15:25:31 +0200

    :sparkles: Add post wrapper around fetch

---
+++ src/lib/utils.js
@@ -0,0 +1,9 @@
+export function post(url, data) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+}
