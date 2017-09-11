fbeefac463dd93025c510387ea1896465145da62
Dominik Kundel <dominik.kundel@gmail.com>
2017-09-11 21:48:22 +0200

    :bug: Fix serving files in prod

--- server/index.js
+++ server/index.js
@@ -18,9 +18,9 @@ app.post('/api/game/submit', game.submit);
 app.post('/api/game/start', game.start);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  app.use(express.static(path.join(__dirname, '..', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
   });
 }
 
