df56fc170151366b716e2db2375853ceba192f2b
Dominik Kundel <dominik.kundel@gmail.com>
2017-09-11 21:50:17 +0200

    :sparkles: Add waiting for judging screen

---
+++ src/pages/wait-judging-screen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Paper from 'material-ui/Paper';
+import List from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+
+import CodeBlock from '../components/code-block';
+import { MutedText } from '../components/common';
+import SubmissionEntry from '../components/submission-entry';
+
+import WaitingScreen from './waiting-screen';
+
+const WaitJudgingScreen = function({ submissions, code }) {
+  const listEntries = submissions.map(s => (
+    <div>
+      <SubmissionEntry key={s.id} hideHeart={true}>
+        {s.comment}
+      </SubmissionEntry>
+      <Divider key={'divider' + s.id} />
+    </div>
+  ));
+  return (
+    <WaitingScreen>
+      <MutedText>
+        The judge is currently the submissions. Here are the submissions:
+      </MutedText>
+      <Paper zDepth={2}>
+        <CodeBlock code={code} />
+      </Paper>
+      <List>{listEntries}</List>
+    </WaitingScreen>
+  );
+};
+
+export default WaitJudgingScreen;
